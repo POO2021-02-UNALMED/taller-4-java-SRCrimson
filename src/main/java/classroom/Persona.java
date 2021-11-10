66126fc4b4852ff2f97a2dad91c7a3319f1e087b
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-11-09 22:48:19 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,47 +2,43 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
-    }
-
+    } //cedula = 3; 
+    
     Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
-    
     Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
-
     Persona(long cedula) {
         this.cedula = cedula;
         this.nombre = "";
         totalPersonas++;
     }
-
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
-    
+    Persona(){	
+    }
     long getCedula() {
         return cedula;
     }
-
     String getNombre() {
         return nombre;
     }
-
-    void setNombre(String nombre) {
+   void setNombre(String nombre) {
         this.nombre = nombre;
     }
 }
