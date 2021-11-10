46ddc31185dd19d48a859de2e688242f268514e0
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-11-09 22:48:13 -0500

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -9,49 +9,40 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre");
-        this(0);
+    }    
+     /*   this(0);
     }
-
     Asignatura(int codigoInterno) {
         this("Sin nombre", codigoInterno, 0);
-    }
-
+    } */
     Asignatura(int codigoExterno) {
         this("Sin nombre", 0, codigoExterno);
     }
-
     Asignatura(String nombre) {
         this(nombre, 0, 0);
     }
-
     Asignatura(String nombre, int codigoInterno, int codigoExterno) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
     }
-
     void cambiarDatos(int codigoInterno, int codigoExterno, String nombre) {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
-    }
-
+    }/*
     void cambiarDatos(int codigoInterno) {
         this.codigoInterno = codigoInterno;
-    }
-
+    }*/
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
-
     void cambiarDatos(String nombre) {
         this.nombre = nombre;
     }
-
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-    
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
